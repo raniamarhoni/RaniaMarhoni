819a2dea060e25be05d593a9312bb4b3532db6ef
raniamarhoni <marhoni@outlook.com>
2020-01-12 14:47:22 +0000

    Scripts bugs fixed

--- index.js
+++ index.js
@@ -3,18 +3,6 @@
 // Modified from a tutorial found at
 // https://www.abeautifulsite.net/smoothly-scroll-to-an-element-without-a-jquery-plugin-2
 
-$('a[href^="#"]').on('click', function(event) {
-
-    var target = $(this.getAttribute('href'));
-
-    if( target.length ) {
-        event.preventDefault();
-        $('html, body').stop().animate({
-            scrollTop: target.offset().top
-        }, 1000);
-    }
-
-});
 
 $('#contact-form').on('submit', function(e) {
     $('#contactFormModal').modal('show');
